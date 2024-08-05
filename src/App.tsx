4d7ecd1b072bf3000831f4ccd695f01d20a15df2
Ari <ariel.cohelo@gmail.com>
2024-08-06 00:44:55 +0200

    layed out all elements,next step is form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './_mixins.scss';
+import './_variables.scss';
+import './_resets.scss';
+import './app.scss';
+import cardBack from './assets/images/bg-card-back.png';
+import cardFront from './assets/images/bg-card-front.png';
+import backDesktop from './assets/images/bg-main-desktop.png';
+import backMobile from './assets/images/bg-main-mobile.png';
+import cardLogo from './assets/images/card-logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="CardDetailsFillUp">
+        <div className="cardViewer">
+          <picture className="cardViewer__background">
+            <source srcSet={backDesktop} media="(min-width:600px)" />
+            <img src={backMobile} alt="mobile-background" />
+          </picture>
+          <div className="cardViewer__cardFront">
+            <img
+              className="cardViewer__cardFront-img"
+              src={cardFront}
+              alt="card-Front"
+            />
+            <img
+              className="cardViewer__cardFront-logo"
+              src={cardLogo}
+              alt="card-logo"
+            />
+            <p className="cardViewer__cardFront-number">0000 0000 0000 0000</p>
+            <p className="cardViewer__cardFront-name">JANE APPLESEED</p>
+            <p className="cardViewer__cardFront-date">00/00</p>
+          </div>
+          <div className="cardViewer__cardBack">
+            <img
+              className="cardViewer__cardBack-img"
+              src={cardBack}
+              alt="card-back"
+            />
+            <p className="cardViewer__cardBack-number">000</p>
+          </div>
+        </div>
+        <div className="cardFormContainer">
+          <form className="cardForm">
+            <label htmlFor="cardName">CARDHOLDER NAME</label>
+            <input type="text" id="cardName" className="cardForm__cardName" />
+            <label htmlFor="cardNumber">CARD NUMBER</label>
+            <input
+              type="number"
+              id="cardNumber"
+              className="cardForm__cardNumber"
+            />
+            <div className="formFlex">
+              <label htmlFor="expDate">EXP. DATE (MM/YY)</label>
+              <input
+                type="number"
+                id="expDate"
+                className="cardForm__cardDateMM"
+              />
+              <label htmlFor="expDateYY"></label>
+              <input
+                type="number"
+                id="expDateYY"
+                className="cardForm__cardDateYY"
+              />
+              <label htmlFor="cvc">CVC</label>
+              <input type="number" className="cardForm__cardCvc" />
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
