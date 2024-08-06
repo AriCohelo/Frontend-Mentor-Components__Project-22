f8f6bb689918216b4208c385f549ceeebf46fbb8
Ari <ariel.cohelo@gmail.com>
2024-08-06 19:02:19 +0200

    finished the UI started with the form validation

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,31 @@ import cardFront from './assets/images/bg-card-front.png';
 import backDesktop from './assets/images/bg-main-desktop.png';
 import backMobile from './assets/images/bg-main-mobile.png';
 import cardLogo from './assets/images/card-logo.svg';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { DevTool } from '@hookform/devtools';
+
+type FormValues = {
+  cardName: string;
+  cardNumber: number;
+  cardDateMM: number;
+  cardDateYY: number;
+  cardCvc: number;
+};
 
 function App() {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit, formState, getValues } = form;
+  const { errors } = formState;
+  const onSubmit = () => {
+    console.log('form submitted');
+  };
   return (
     <>
       <div className="CardDetailsFillUp">
         <div className="cardViewer">
           <picture className="cardViewer__background">
-            <source srcSet={backDesktop} media="(min-width:600px)" />
+            <source srcSet={backDesktop} media="(min-width:650px)" />
             <img src={backMobile} alt="mobile-background" />
           </picture>
           <div className="cardViewer__cardFront">
@@ -42,32 +59,62 @@ function App() {
           </div>
         </div>
         <div className="cardFormContainer">
-          <form className="cardForm">
+          <form
+            className="cardForm"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <label htmlFor="cardName">CARDHOLDER NAME</label>
-            <input type="text" id="cardName" className="cardForm__cardName" />
+            <input
+              type="text"
+              id="cardName"
+              className="cardForm__cardName"
+              placeholder="e.g. Jane Appleseed"
+              {...register('cardName', {
+                required: 'This Field is required',
+              })}
+            />
+            <p className="cardForm__errors">{errors.cardName?.message}</p>
             <label htmlFor="cardNumber">CARD NUMBER</label>
             <input
               type="number"
               id="cardNumber"
               className="cardForm__cardNumber"
+              placeholder="e.g. 1234 5678 9123 0000"
+              {...register('cardNumber')}
             />
-            <div className="formFlex">
-              <label htmlFor="expDate">EXP. DATE (MM/YY)</label>
+            <div className="cardForm__groupNumbers">
+              <label htmlFor="expDate" className="cardForm__expDateLab">
+                EXP. DATE (MM/YY)
+              </label>
               <input
                 type="number"
                 id="expDate"
                 className="cardForm__cardDateMM"
+                placeholder="MM"
+                {...register('cardDateMM')}
               />
-              <label htmlFor="expDateYY"></label>
               <input
                 type="number"
                 id="expDateYY"
                 className="cardForm__cardDateYY"
+                placeholder="YY"
+                {...register('cardDateYY')}
               />
-              <label htmlFor="cvc">CVC</label>
-              <input type="number" className="cardForm__cardCvc" />
+              <label htmlFor="cvc" className="cardForm__cardCvcLab">
+                CVC
+              </label>
+              <input
+                type="number"
+                id="cvc"
+                className="cardForm__cardCvc"
+                placeholder="e.g. 123"
+                {...register('cardCvc')}
+              />
             </div>
+            <button className="cardForm__button">Confirm</button>
           </form>
+          <DevTool control={control} />
         </div>
       </div>
     </>
